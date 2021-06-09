cb438aa690b2eccac7e02a4194790998d31217fc
BeneZ <bezraat@gmail.com>
2021-06-09 15:32:25 +0200

    stream-demo test

--- streams-demo-public/streams-demo/src/test/java/streams/OrderServiceTest.java
+++ streams-demo-public/streams-demo/src/test/java/streams/OrderServiceTest.java
@@ -5,7 +5,9 @@ import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
+import static org.assertj.core.api.Assertions.allOf;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -24,12 +26,12 @@ class OrderServiceTest {
         Product p4 = new Product("Lord of The Rings", "Book", 20);
         Product p5 = new Product("Harry Potter Collection", "Book", 120);
 
-        Order o1 = new Order("pending", LocalDate.of(2021, 06, 07));
+        Order o1 = new Order("pending", LocalDate.of(2021, 6, 7));
         o1.addProduct(p1);
         o1.addProduct(p2);
         o1.addProduct(p5);
 
-        Order o2 = new Order("on delivery", LocalDate.of(2021, 06, 01));
+        Order o2 = new Order("on delivery", LocalDate.of(2021, 6, 1));
         o2.addProduct(p3);
         o2.addProduct(p1);
         o2.addProduct(p2);
@@ -69,10 +71,10 @@ class OrderServiceTest {
         assertThat(result)
                 .hasSize(2)
                 .extracting(Product::getName)
-                .containsExactly("Tv","Laptop");
+                .containsExactly("Tv", "Laptop");
 
         assertEquals(2, result.size());
-        assertEquals("Tv",result.get(0).getName());
+        assertEquals("Tv", result.get(0).getName());
 
     }
 
@@ -80,7 +82,47 @@ class OrderServiceTest {
     void productOverAmountPriceOver2500() {
         List<Product> result = ordersService.productOverAmountPrice(2500);
 
-         assertEquals(0, result.size());
+        assertEquals(0, result.size());
+    }
+
+    @Test
+    void testSumProductsPriceBetweenTime() {
+        double result = ordersService.sumProductsPriceBetweenTime(LocalDate.of(2021, 5, 1),
+                LocalDate.of(2021, 6, 8));
+        assertEquals(9320d, result, 0.01);
+    }
+
+    @Test
+    void testSumProductsPriceBetweenTime2() {
+        double result = ordersService.sumProductsPriceBetweenTime(LocalDate.of(2021, 6, 6),
+                LocalDate.of(2021, 6, 8));
+        assertEquals(4520d, result, 0.01);
+    }
+
+    @Test
+    void findProductByName() {
+        Optional<Product> result = ordersService.findProductByName("Tv");
+        assertTrue(result.isPresent());
+        assertEquals("Tv", result.get().getName());
+    }
+
+    @Test
+    void findProductByNameNotFound() {
+        Optional<Product> result = ordersService.findProductByName("Tv2");
+        assertTrue(result.isEmpty());
+    }
+
+    @Test
+    void findOrderMostExpensive() {
+        Optional<Order> result = ordersService.findOrderMostExpensive();
+        assertTrue(result.isPresent());
+
+        assertThat(result.get().getProducts())
+                .hasSize(3)
+                .extracting(Product::getName)
+                .contains("Tv", "Laptop", "Harry Potter Collection");
+
+
     }
 
 }
