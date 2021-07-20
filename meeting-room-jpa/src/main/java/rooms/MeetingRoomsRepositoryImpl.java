85b61c85ffeed08cfaec0366935bca0d57b41967
BeneZ <bezraat@gmail.com>
2021-07-20 16:54:40 +0200

    Órai feladat - 2021.07.20 Jpa all method and test

--- meeting-room-jpa/src/main/java/rooms/MeetingRoomsRepositoryImpl.java
+++ meeting-room-jpa/src/main/java/rooms/MeetingRoomsRepositoryImpl.java
@@ -4,7 +4,9 @@ import lombok.RequiredArgsConstructor;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.TypedQuery;
 import java.util.List;
+import java.util.stream.IntStream;
 
 @RequiredArgsConstructor
 public class MeetingRoomsRepositoryImpl implements MeetingRoomsRepository {
@@ -25,34 +27,61 @@ public class MeetingRoomsRepositoryImpl implements MeetingRoomsRepository {
     @Override
     public List<String> getMeetingroomsOrderedByName() {
         EntityManager em = entityManagerFactory.createEntityManager();
-        List<String> roomsName = em.createQuery("select m.name from MeetingRoom m order by m.name",String.class)
-        .getResultList();
+        List<String> roomsName = em.createQuery("select m.name from MeetingRoom m order by m.name", String.class)
+                .getResultList();
         em.close();
         return roomsName;
     }
 
     @Override
     public List<String> getEverySecondMeetingRoom() {
-        return null;
+        EntityManager em = entityManagerFactory.createEntityManager();
+        List<String> roomsName = em.createQuery("select m.name from MeetingRoom m order by m.name", String.class)
+                .getResultList();
+        em.close();
+        return IntStream.range(0, roomsName.size())
+                .filter(n -> n % 2 == 1)
+                .mapToObj(roomsName::get)
+                .toList();
+
     }
 
     @Override
     public List<MeetingRoom> getMeetingRooms() {
-        return null;
+        EntityManager em = entityManagerFactory.createEntityManager();
+        List<MeetingRoom> meetingRooms = em.createQuery("select m from MeetingRoom m order by m.name", MeetingRoom.class)
+                .getResultList();
+        em.close();
+        return meetingRooms;
     }
 
     @Override
     public List<MeetingRoom> getExactMeetingRoomByName(String name) {
-        return null;
+        EntityManager em = entityManagerFactory.createEntityManager();
+        List<MeetingRoom> meetingRooms = em.createQuery("select m from MeetingRoom m where m.name= :name order by m.name", MeetingRoom.class)
+                .setParameter("name", name)
+                .getResultList();
+        em.close();
+        return meetingRooms;
     }
 
     @Override
-    public List<MeetingRoom> getMeetingRoomsByPrefix(String nameOrPrefix) {
-        return null;
+    public List<MeetingRoom> getMeetingRoomsByPrefix(String name) {
+        EntityManager em = entityManagerFactory.createEntityManager();
+        List<MeetingRoom> meetingRooms = em.createQuery("select m from MeetingRoom m where m.name like :name order by m.name", MeetingRoom.class)
+                .setParameter("name", name + "%")
+                .getResultList();
+        em.close();
+        return meetingRooms;
     }
 
     @Override
     public void deleteAll() {
-
+        EntityManager em = entityManagerFactory.createEntityManager();
+        em.getTransaction().begin();
+        em.createQuery("delete from MeetingRoom")
+                .executeUpdate();
+        em.getTransaction().commit();
+        em.close();
     }
 }
--- meeting-room-jpa/src/test/java/rooms/MeetingRoomsRepositoryImplIT.java
+++ meeting-room-jpa/src/test/java/rooms/MeetingRoomsRepositoryImplIT.java
@@ -22,12 +22,68 @@ class MeetingRoomsRepositoryImplIT {
 
     @Test
     void saveAndGet() {
-        meetingRoomsRepository.save("name1",1,2);
-        meetingRoomsRepository.save("name2",2,4);
+        meetingRoomsRepository.save("name1", 1, 2);
+        meetingRoomsRepository.save("name2", 2, 4);
         List<String> result = meetingRoomsRepository.getMeetingroomsOrderedByName();
 
         assertEquals(2, result.size());
-        assertEquals("name1",result.get(0));
+        assertEquals("name1", result.get(0));
+    }
+
+    @Test
+    void getEverySecondMeetingRoom() {
+        meetingRoomsRepository.save("nameA1", 1, 2);
+        meetingRoomsRepository.save("nameB2", 2, 4);
+        meetingRoomsRepository.save("nameC3", 3, 6);
+        meetingRoomsRepository.save("nameD4", 4, 8);
+
+        List<String> result = meetingRoomsRepository.getEverySecondMeetingRoom();
+        assertEquals(2, result.size());
+        assertEquals(List.of("nameB2", "nameD4"), result);
+    }
+
+    @Test
+    void getMeetingRooms() {
+        meetingRoomsRepository.save("nameA1", 1, 2);
+        meetingRoomsRepository.save("nameB2", 2, 4);
+
+        List<MeetingRoom> result = meetingRoomsRepository.getMeetingRooms();
+        assertEquals(2, result.size());
+        assertEquals("nameA1", result.get(0).getName());
+    }
+
+    @Test
+    void getExactMeetingRoomByName() {
+        meetingRoomsRepository.save("nameA1", 1, 2);
+        meetingRoomsRepository.save("nameA1", 2, 4);
+        meetingRoomsRepository.save("nameA13", 3, 6);
+
+        List<MeetingRoom> result = meetingRoomsRepository.getExactMeetingRoomByName("nameA1");
+        assertEquals(2, result.size());
+        assertEquals("nameA1", result.get(0).getName());
+    }
+
+    @Test
+    void getMeetingRoomsByPrefix() {
+        meetingRoomsRepository.save("nameA1", 1, 2);
+        meetingRoomsRepository.save("nameA1", 2, 4);
+        meetingRoomsRepository.save("nameA13", 3, 6);
+
+        List<MeetingRoom> result = meetingRoomsRepository.getMeetingRoomsByPrefix("nameA1");
+        assertEquals(3, result.size());
+        assertEquals("nameA1", result.get(0).getName());
+    }
+
+    @Test
+    void deleteAll() {
+        meetingRoomsRepository.save("nameA1", 1, 2);
+        meetingRoomsRepository.save("nameA1", 2, 4);
+        meetingRoomsRepository.save("nameA13", 3, 6);
+
+        meetingRoomsRepository.deleteAll();
+        List<MeetingRoom> meetingRooms = meetingRoomsRepository.getMeetingRooms();
+
+        assertEquals(0,meetingRooms.size());
     }
 
 
